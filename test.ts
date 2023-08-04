f3381f913f511c12adf190ba4efe14ce7c7fb56a
PLENTYZ\JsIntern2 <usman.chh2001@gmail.com>
2023-08-04 13:26:25 +0500

    typescript course

---
+++ test.ts
@@ -0,0 +1,12 @@
+// const add=(a:number,b:number)=>{
+//     console.log(a+b);
+// }
+
+// add(3,6)
+
+//const num:number=15000;
+
+//const isTrue:boolean=true;
+
+
+// ts-node is required for typescript 
